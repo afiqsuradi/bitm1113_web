8ca5c0dc877583222096a18412e12131df5975c4
Aiman Afiq <aimanafiq05@gmail.com>
2024-12-07 20:59:39 +0800

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,17 +8,19 @@ type CardProps = {
 }
 
 export default function Card({ children, icon, iconAlt, title }: CardProps) {
-    <div className="bg-white rounded-2xl  drop-shadow-xl max-w-sm text-[#3A0018] flex flex-col items-center overflow-hidden py-12 gap-12">
-        <div className="p-6 bg-[#FFEDDE] rounded-full w-fit h-full">
-            <Image src={icon} alt={iconAlt} width={80} />
+    return (
+        <div className="bg-white rounded-2xl  drop-shadow-xl max-w-sm text-[#3A0018] flex flex-col items-center overflow-hidden py-12 gap-12">
+            <div className="p-6 bg-[#FFEDDE] rounded-full w-fit h-full">
+                <Image src={icon} alt={iconAlt} width={80} />
+            </div>
+            <div className="px-6">
+                <h2>
+                    {title}
+                </h2>
+                <p>
+                    {children}
+                </p>
+            </div>
         </div>
-        <div className="px-6">
-            <h2>
-                {title}
-            </h2>
-            <p>
-                {children}
-            </p>
-        </div>
-    </div>
+    )
 }
