97236feaeef5ea0c03c472f1f17f228abd7b7f53
Aiman Afiq <aimanafiq05@gmail.com>
2024-12-07 22:08:29 +0800

    added design principle section

---
+++ src/app/_sections/design-principle.tsx
@@ -0,0 +1,25 @@
+
+import ContrastIcon from "../../../public/icons/contrast.svg"
+import EmphasisIcon from "../../../public/icons/emphasis.svg"
+import UnityIcon from "../../../public/icons/unity.svg"
+import Card from "@/components/card";
+
+export function DesignPrinciple() {
+    return (
+        <div className="container mx-auto z-10">
+            <h1 className="font-bold text-5xl mb-6 text-[#3A0018] text-center">Design Principle</h1>
+            <div className="grid auto-rows-auto max-lg:gap-12 lg:grid-cols-3 place-items-center p-6">
+                <Card icon={ContrastIcon} iconAlt="Contrast icon" title="Contrast">
+                    The contrast between the light skin tone and the dark red teardrop mirrors the duality of human nature,
+                    symbolizing the interplay between light and darkness, joy and sorrow.
+                </Card>
+                <Card icon={EmphasisIcon} iconAlt="Emphasis icon" title="Emphasis">
+                    The teardrop, as the focal point of the logo, draws the viewer&apos;s attention and highlights the emotional intensity of the character.
+                </Card>
+                <Card icon={UnityIcon} iconAlt="Unity icon" title="Unity">
+                    The overall design is unified by the use of a consistent color palette and a simple, clean aesthetic.
+                </Card>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import About from "./_sections/about";
+import { DesignPrinciple } from "./_sections/design-principle";
 import Hero from "./_sections/hero";
 
 
@@ -8,13 +10,25 @@ export default function Home() {
 
         <Hero />
       </section>
-      <div className="custom-shape-divider-bottom-1733558555">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
-        </svg>
+      <div className="relative">
+        <div className="custom-shape-divider-bottom-1733558555">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+          </svg>
+        </div>
       </div>
       <section className="bg-[#FFE2C5]">
-
+        <About />
+      </section>
+      <div className="relative pt-18">
+        <div className="custom-shape-divider-top-1733580032">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="fill-[#FFE2C5]"></path>
+          </svg>
+        </div>
+      </div>
+      <section className="mt-64">
+        <DesignPrinciple />
       </section>
     </div>
   );
